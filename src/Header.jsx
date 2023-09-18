28a71b6b2985556ade67f261827d11a88959645d
njokafor <ok4nnaemeka@gmail.com>
2023-09-18 03:30:58 -0400

    added imge to public

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <div className="points-header">
-            <img className="uk-nsbe-img" src="./src/ukNsbeCroped3.png" alt="UKNSBE"/>
+            <img className="uk-nsbe-img" src="../src/ukNsbeCroped3.png" alt="UKNSBE"/>
             <p className="points-title">Points Tracking</p>
         </div>
     )
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Points_Tracking/",
+  //base: "/Points_Tracking/",
+  base: process.env.NODE_ENV === 'production' ? '/Points_Tracking/' : '/',
   plugins: [react()],
 })
